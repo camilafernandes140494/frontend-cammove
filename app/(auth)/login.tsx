0a1592cc7722659cc3740f0fcd7b71314f8c891e
Camila <camilaferna140494@gmail.com>
2025-01-20 17:40:27 -0300

    fix: correction layout

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { useTranslationContext } from "../TranslationContext";
-// import Button from "@/components/Button";
-import { useTheme } from "../ThemeContext";
-import FormikTextInput from "@/components/FormikTextInput";
-import { postLogin } from "@/api/auth/auth.api";
 import { useRouter } from "expo-router";
+import { TextInput, Button, HelperText, Text, Snackbar } from "react-native-paper";
+import { postLogin } from "@/api/auth/auth.api";
 import { useUser } from "../UserContext";
-import { Button } from 'react-native-paper';
+import { useTheme } from "../ThemeContext";
 
 const Login = () => {
-    const { theme } = useTheme();
-    const { t } = useTranslationContext();
     const router = useRouter();
     const { setUser } = useUser();
+    const { theme } = useTheme();
+
+    const [visible, setVisible] = useState(false);
+
 
     const validationSchema = Yup.object().shape({
         email: Yup.string()
@@ -32,80 +31,136 @@ const Login = () => {
             setUser({ id: userCredential.user_id, email: '', name: "" })
             router.push('/home')
         } catch (error) {
-            console.error("Erro ao logar:", error);
+            setVisible(true);
         }
     };
 
+
+
+
     return (
-        <View style={{ flex: 1, backgroundColor: theme.background }}>
-            <View
-                style={{
-                    position: "absolute",
-                    width: 300,
-                    height: 300,
-                    backgroundColor: theme.secondary,
-                    borderRadius: 150,
-                    top: -80,
-                    left: -80,
-                }}
-            />
-            <View
-                style={{
-                    position: "absolute",
-                    width: 200,
-                    height: 200,
-                    backgroundColor: theme.secondary,
-                    borderRadius: 100,
-                    bottom: -30,
-                    right: -30,
-                }}
-            />
+        <View style={{ flex: 1, backgroundColor: "#fff" }}>
             <View
                 style={{
                     flex: 1,
+                    backgroundColor: theme.primary,
                     justifyContent: "center",
                     alignItems: "center",
-                    padding: 20,
+                    paddingBottom: 5,
                 }}
             >
-                <Text
-                    style={{
-                        fontSize: 24,
-                        fontWeight: "bold",
-                        marginBottom: 20,
-                        color: theme.textPrimary,
-                    }}
-                >
-                    {t("welcome")}
+                <Text variant="displayMedium" style={{ color: theme.background }} >
+                    CAMMOVE
                 </Text>
+            </View>
+
+            <View
+                style={{
+                    flex: 2,
+                    marginTop: -50,
+                    backgroundColor: theme.background,
+                    borderTopEndRadius: 40,
+                    borderTopStartRadius: 40,
+                    padding: 20,
+                    paddingTop: 50,
+                }}
+            >
+                <Snackbar
+                    visible={visible}
+                    onDismiss={() => setVisible(false)}
+                    action={{
+                        label: "",
+                        icon: 'close',
+                        onPress: () => setVisible(false),
+                    }}>
+                    Erro ao logar
+                </Snackbar>
                 <Formik
                     initialValues={{ email: "", password: "" }}
                     validationSchema={validationSchema}
                     onSubmit={handleLogin}
                 >
-                    {({ handleSubmit }) => (
-                        <View style={{ width: "100%" }}>
-
-                            <FormikTextInput name="email" placeholder={"E-mail"} />
-                            <FormikTextInput
-                                name="password"
-                                placeholder={"Senha"}
-                                secureTextEntry
+                    {({
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        values,
+                        errors,
+                        touched,
+                    }) => (
+                        <View style={{
+                            display: 'flex', flexDirection: 'column', gap: 5
+                        }}>
+                            < TextInput
+                                mode="flat"
+                                label="E-mail"
+                                value={values.email}
+                                onChangeText={handleChange("email")}
+                                onBlur={handleBlur("email")}
+                                style={{
+                                    backgroundColor: theme.background,
+                                }}
+                                error={touched.email && Boolean(errors.email)}
                             />
+                            {touched.email && errors.email && (
+                                <HelperText type="error" >
+                                    {errors.email}
+                                </HelperText>
+                            )}
+                            <TextInput
+                                mode="flat"
+                                label="Senha"
+                                value={values.password}
+                                onChangeText={handleChange("password")}
+                                onBlur={handleBlur("password")}
+                                secureTextEntry
+                                right={<TextInput.Icon icon="eye" />}
+                                style={{
+                                    backgroundColor: theme.background,
+                                }}
+                                error={touched.password && Boolean(errors.password)}
+                            />
+                            {touched.password && errors.password && (
+                                <HelperText type="error" >
+                                    {errors.password}
+                                </HelperText>
+                            )}
 
 
-                            <Button mode="contained" onPress={handleSubmit as any}>
-                                {t("enter")}
+                            <Button style={{
+                                alignSelf: "flex-end",
+                                marginBottom: 20,
+                            }} mode="text" onPress={() => console.log('Pressed')}>
+                                Esqueceu a senha?
                             </Button>
+                            <Button
+                                mode="contained"
+                                onPress={handleSubmit as any}
+                                style={{
+                                    borderRadius: 10,
+                                    marginVertical: 20,
+                                }}
+                                contentStyle={{ height: 50 }}
+                            >
+                                ENTRAR
+                            </Button>
+
+
+                            <Text style={{ textAlign: "center", color: "#6c757d" }}>
+                                Não tem uma conta?{" "}
+                                <Text
+                                    style={{ fontWeight: "bold" }}
+                                    onPress={() => router.push("/createUser")}
+                                >
+                                    Cadastre-se
+                                </Text>
+                            </Text>
                         </View>
                     )}
                 </Formik>
-                <Button mode="contained" onPress={() => router.push('/createUser')}>
-                    Não tem uma conta? Cadastre-se
-                </Button>
 
-            </View>
-        </View>
+            </View >
+        </View >
     );
 };
 
--- app/ThemeContext.tsx
+++ app/ThemeContext.tsx
@@ -10,33 +10,39 @@ interface ThemeProviderProps {
 }
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-    const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     const toggleTheme = () => setIsDarkMode(prevMode => !prevMode);
 
-    const theme = {
-        dark: {
-            primary: "#8A2BE2", // Roxo Médio (forte, criativo e com bastante presença)
-            secondary: "#cc96cc", // Rosa Vivid (vivaz e marcante, complementando o roxo)
-            background: "#1A1A2E", // Azul Escuro Profundo (sofisticado e ideal para leitura em fundo escuro)
-            textButton: "#FFFFFF", // Branco (contraste claro e limpo)
-            textPrimary: "#EAEAEA", // Cinza Muito Claro (legibilidade em fundos escuros)
-            textSecondary: "#B5B5C3", // Cinza Médio (neutro e suave)
-            success: "#7AC074", // Verde Menta (energia positiva e saúde)
-            error: "#F28C8C", // Rosa Coral Suave (sinalização de erro clara, mas amigável)
-        },
-        light: {
-            primary: "#9B59B6", // Roxo Claro (forte e criativo)
-            secondary: "#D4A5D9", // Lilás (delicado e acolhedor)
-            background: "#F0F0F0", // Cinza Claro (para fundo claro e confortável)
-            textButton: "#FFFFFF", // Branco (para contraste com botões coloridos)
-            textPrimary: "#2C2C34", // Grafite Suave (legibilidade em fundos claros)
-            textSecondary: "#73737D", // Cinza Neutro (complementar ao grafite)
-            success: "#A8E6CF", // Verde Pastel Claro (sensação de saúde e bem-estar)
-            error: "#FF6F61", // Vermelho Coral Suave (alerta, mas sem ser agressivo)
-        },
+    const colorsCommon = {
+        white: "#FFFFFF", // Cor branca comum
+        black: "#000000", // Cor preta comum
     };
 
+    const theme = {
+        dark: {
+            ...colorsCommon,
+            primary: "#6A4C9C", // Roxo Suave
+            secondary: "#B8A9C9", // Lilás Suave
+            background: "#aaaaaa", // Azul Escuro Profundo
+            textButton: "#FFFFFF",
+            textPrimary: "#E5E5E5", // Cinza Claro
+            textSecondary: "#B5B5C3", // Cinza Médio
+            success: "#7AC074", // Verde Menta
+            error: "#F28C8C", // Rosa Coral Suave
+        },
+        light: {
+            ...colorsCommon,
+            primary: "#D6A4D4", // Lavanda Claro
+            secondary: "#D4A5D9", // Lilás Delicado
+            background: "#F0F0F0", // Cinza Claro
+            textButton: "#FFFFFF",
+            textPrimary: "#3C3C3C", // Cinza Médio
+            textSecondary: "#73737D", // Cinza Neutro
+            success: "#A8E6CF", // Verde Pastel Claro
+            error: "#FF6F61", // Vermelho Coral Suave
+        },
+    };
     return (
         <ThemeContext.Provider value={{ theme: isDarkMode ? theme.dark : theme.light, toggleTheme }}>
             {children}
