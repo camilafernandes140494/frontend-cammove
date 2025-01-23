8959b9198f740e9da4e93ddb7401a7f4500e4411
Camila <camilaferna140494@gmail.com>
2025-01-23 18:10:29 -0300

    fix: add new endpoint

--- api/users/users.api.ts
+++ api/users/users.api.ts
@@ -2,9 +2,9 @@
 import api from "../axios";
 import { PostUser } from "./users.types";
 
-export const postUser = async (params: PostUser) => {
+export const postUser = async (IdUser: string, params: PostUser) => {
   try {
-    const response = await api.post("/users", params);
+    const response = await api.post(`/users/${IdUser}`, params);
     return response.data; // Retorna os dados da resposta
   } catch (error) {
     console.error("Erro no login:", error);
@@ -14,7 +14,17 @@ export const postUser = async (params: PostUser) => {
 
 export const patchUser = async (IdUser: string, params: PostUser) => {
   try {
-    const response = await api.post(`/users/${IdUser}`, params);
+    const response = await api.patch(`/users/${IdUser}`, params);
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error("Erro no login:", error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
+
+export const getUserById = async (IdUser: string) => {
+  try {
+    const response = await api.get<PostUser>(`/users/${IdUser}`);
     return response.data; // Retorna os dados da resposta
   } catch (error) {
     console.error("Erro no login:", error);
