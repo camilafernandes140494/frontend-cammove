fa41aef30b66169207c169026f5d041bbd2b9ca3
Camila <camilaferna140494@gmail.com>
2025-01-28 17:44:56 -0300

    feat: create types GENDER | PERMISSION

--- api/users/users.types.ts
+++ api/users/users.types.ts
@@ -1,6 +1,9 @@
+export type PERMISSION = "ADMIN" | "STUDENT" | "TEACHER" | null;
+export type GENDER = "MALE" | "FEMALE" | "OTHER" | "PREFER_NOT_TO_SAY" | null;
+
 export type PostUser = {
   name: string;
-  gender: string;
+  gender: GENDER;
   birthDate: string;
-  permission: string;
+  permission: PERMISSION;
 };
