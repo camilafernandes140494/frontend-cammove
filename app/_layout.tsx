4d07c433819159cb9211bce24125d3f609c21693
Camila <camilaferna140494@gmail.com>
2025-01-29 11:09:13 -0300

    fix: remove useRouter

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,14 +16,17 @@ import Login from './(auth)/login';
 import Home from './(home)/home';
 import createUser from './(auth)/createUser';
 import Onboarding from './(onboarding)/onboarding';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Workouts from './(workouts)/workouts';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 export default function RootLayout() {
-  const router = useRouter();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -37,6 +40,36 @@ export default function RootLayout() {
       SplashScreen.hideAsync(); // Esconde o splash screen após o carregamento das fontes
     }
   }, [loaded]);
+
+  function HomeTabs() {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          // tabBarIcon: ({ color, size }) => {
+          //   let iconName;
+
+          //   if (route.name === "Home") {
+          //     iconName = "home";
+          //   } else if (route.name === "Works") {
+          //     iconName = "briefcase";
+          //   } else if (route.name === "Training") {
+          //     iconName = "barbell";
+          //   }
+
+          //   return <Ionicons name={iconName} size={size} color={color} />;
+          // },
+          tabBarActiveTintColor: "#6200ea",
+          tabBarInactiveTintColor: "gray",
+          headerShown: false, // Esconder o header superior
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Workouts" component={Workouts} />
+
+      </Tab.Navigator>
+    );
+  }
+
 
   // A tela não pode ser renderizada até que as fontes estejam carregadas
   if (!loaded) {
@@ -54,8 +87,8 @@ export default function RootLayout() {
               <Stack.Navigator>
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Register" component={createUser} />
-                <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Onboarding" component={Onboarding} />
+                <Stack.Screen name="Home" component={HomeTabs} options={{ headerShown: false }} />
 
               </Stack.Navigator>
             </TranslationProvider>
