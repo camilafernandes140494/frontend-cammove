d94c17175169f5bdf2b3cf2ab37e4994d010469c
Camila <camilaferna140494@gmail.com>
2025-01-29 22:40:31 -0300

    feat: create exercises page

---
+++ app/(exercises)/exercises.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ScrollView, View, } from 'react-native';
+import { Appbar, Text, } from 'react-native-paper';
+import { useUser } from '../UserContext';
+
+
+const Exercises = () => {
+    const { user } = useUser();
+
+    return (
+        <View style={{ flex: 1 }}>
+            <Appbar.Header>
+                <Appbar.BackAction onPress={() => { }} />
+                <Appbar.Content title="Title" />
+                <Appbar.Action icon="calendar" onPress={() => { }} />
+                <Appbar.Action icon="magnify" onPress={() => { }} />
+            </Appbar.Header>
+            <ScrollView
+                style={{ flex: 1 }}
+                contentContainerStyle={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    padding: 50,
+                }}
+            >
+
+                <Text variant="headlineMedium" style={{ textAlign: 'center' }}>
+                    Olá {user.name}
+                </Text>
+
+                {/* <Skeleton style={{ width: '90%', height: 500, borderRadius: 10 }} /> */}
+            </ScrollView>
+        </View>
+
+    );
+};
+
+export default Exercises;
