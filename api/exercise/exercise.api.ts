94849e2ab4eb413df4ebfda5e56781651b922f55
Camila <camilaferna140494@gmail.com>
2025-02-04 23:18:41 -0300

    feat: add exercise endpoint

---
+++ api/exercise/exercise.api.ts
@@ -0,0 +1,23 @@
+import api from '../axios';
+import { Exercise } from './exercise.types';
+
+export const postExercise = async (params: Exercise) => {
+  try {
+    const response = await api.post(`/exercises`, params);
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro no login:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
+
+export const getExercises = async (params?: Record<string, string>) => {
+  try {
+    const response = await api.get<Exercise[]>(`/exercises`, { params });
+    console.log(response, 'response');
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro ao carregar exercícios:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
--- api/exercise/exercise.types.ts
+++ api/exercise/exercise.types.ts
@@ -1,9 +1,10 @@
 export type Exercise = {
   name: string;
   description: string;
-  type: string;
-  images: string[];
-  createdAt: string;
+  category?: string;
+  muscleGroup?: string[];
+  images?: string[];
+  createdAt?: string;
   updatedAt?: string;
   deletedAt?: string;
 };
