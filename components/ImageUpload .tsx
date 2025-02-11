253b5fa4e287c64601b91b630893106e35398cd6
Camila <camilaferna140494@gmail.com>
2025-02-10 21:26:52 -0300

    fix: correction ImageUpload component

--- components/ImageUpload .tsx
+++ components/ImageUpload .tsx
@@ -2,14 +2,12 @@ import { useState } from 'react';
 import { Image, View, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { postUpload } from '@/api/exercise/exercise.api';
-import { Upload } from '@/api/exercise/exercise.types';
 import { Button } from 'react-native-paper';
 
 export default function ImageUpload() {
     const [image, setImage] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState(false);
 
-    console.log(image)
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ['images', 'videos'],
@@ -18,33 +16,37 @@ export default function ImageUpload() {
             quality: 1,
         });
 
-        console.log(result);
-
         if (!result.canceled) {
             setImage(result.assets[0].uri);
         }
     };
 
     const handleSave = async () => {
+        setIsLoading(true)
+
         if (image) {
             // Fazendo a conversão de URI para Blob
             const response = await fetch(image);
             const blob = await response.blob();
 
-            // Criando um objeto File com o Blob
-            const file = new File([blob], "uploaded-image.jpg", { type: blob.type });
-
             // Criação do FormData
             const formData = new FormData();
-            formData.append('file', file);  // Aqui estamos enviando o file diretamente no FormData
-            formData.append('folder', 'exercises');  // Outros dados podem ser anexados no FormData
+            formData.append('file', {
+                uri: image,
+                name: 'uploaded-file.jpg',
+                type: blob.type
+            } as any);
+            formData.append('folder', 'exercises');
 
-            // Enviando o FormData para o backend
             try {
-                const response = await postUpload(formData);  // Modificado para passar o FormData diretamente
+                const response = await postUpload(formData);
                 console.log('Upload bem-sucedido:', response);
             } catch (error) {
                 console.error('Erro ao upload de arquivo:', error);
+            }
+            finally {
+                setIsLoading(false)
+
             }
         } else {
             console.log('Nenhuma imagem selecionada');
@@ -53,22 +55,11 @@ export default function ImageUpload() {
 
 
     return (
-        <View style={styles.container}>
-            <Button onPress={pickImage} >Escolher uma imagem da galeria</Button>
-            {image && <Image source={{ uri: image }} style={styles.image} />}
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+            <Button onPress={pickImage}>Escolher uma imagem </Button>
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200, }} />}
             <Button onPress={handleSave} loading={isLoading}>Salvar</Button>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    image: {
-        width: 200,
-        height: 200,
-    },
-});
