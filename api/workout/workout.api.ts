b0477895e175755f2d6e2b2de0b2c9094fd32cb8
Camila <camilaferna140494@gmail.com>
2025-02-20 22:21:33 -0300

    feat: add new endpoint

--- api/workout/workout.api.ts
+++ api/workout/workout.api.ts
@@ -36,3 +36,28 @@ export const getWorkoutsSummary = async (
     throw error; // Propaga o erro para quem chamar a função
   }
 };
+
+export const getWorkoutsByStudentId = async (studentId: string) => {
+  try {
+    const response = await api.get<string[]>(`/workouts/students/${studentId}`);
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro no login:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
+
+export const getWorkoutByStudentIdAndWorkoutId = async (
+  workoutsId: string,
+  studentId: string,
+) => {
+  try {
+    const response = await api.get<WorkoutData>(
+      `/workouts/${workoutsId}/students/${studentId}`,
+    );
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro no login:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
