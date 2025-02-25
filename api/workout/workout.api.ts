b6ae6a629032036944f235b2fdee32e1692a93a6
Camila <camilaferna140494@gmail.com>
2025-02-24 23:01:37 -0300

    feat: add new endpoints

--- api/workout/workout.api.ts
+++ api/workout/workout.api.ts
@@ -19,6 +19,24 @@ export const postWorkout = async (
   }
 };
 
+export const patchWorkout = async (
+  workoutId: string,
+  teacherId: string,
+  studentId: string,
+  params: Partial<WorkoutData>,
+) => {
+  try {
+    const response = await api.patch(
+      `/workouts/${workoutId}/students/${studentId}/teacher/${teacherId}`,
+      params,
+    );
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro ao criar treino:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
+
 export const getWorkoutsSummary = async (
   teacherId: string,
   params?: Record<string, string>,
@@ -44,6 +62,38 @@ export const getWorkoutsByStudentId = async (studentId: string) => {
   } catch (error) {
     console.error('Erro no login:', error);
     throw error; // Propaga o erro para quem chamar a função
+  }
+};
+
+export const deleteWorkoutsByStudentId = async (
+  workoutId: string,
+  studentId: string,
+  teacherId: string,
+) => {
+  try {
+    const response = await api.delete(
+      `/workouts/${workoutId}/students/${studentId}/teacher/${teacherId}`,
+    );
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro no login:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
+
+export const duplicateWorkout = async (
+  workoutId: string,
+  studentId: string,
+  teacherId: string,
+) => {
+  try {
+    const response = await api.post(
+      `/workouts/${workoutId}/students/${studentId}/teachers/${teacherId}/duplicate`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Erro no login:', error);
+    throw error;
   }
 };
 
