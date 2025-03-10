70f3ac55e065e99dbf4692b7d8dc7a34d7f6955f
Camila <camilaferna140494@gmail.com>
2025-03-10 18:00:45 -0300

    feat: add table in GeneratePDFBase64

--- common/GeneratePDFBase64.tsx
+++ common/GeneratePDFBase64.tsx
@@ -1,13 +1,31 @@
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
 
 pdfMake.vfs = pdfFonts.vfs;
 
-const GeneratePDFBase64 = async (): Promise<string> => {
+const GeneratePDFBase64 = async (tableData?: string[][]): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const docDefinition = {
+    const docDefinition: TDocumentDefinitions = {
       content: [
-        { text: 'Este é um PDF gerado automaticamente!', fontSize: 14 },
+        { text: 'Avaliação fisica', fontSize: 14, margin: [0, 10, 0, 0], alignment: 'center', },
+        {
+          table: {
+            headerRows: 1, // número de linhas de cabeçalho
+            widths: ['*', '*'], // define a largura das colunas
+            body: [
+              ...tableData ?? [],  // Espalha o conteúdo da tabela recebido como argumento
+            ],
+          },
+        },
+      ],
+      footer: (currentPage: number, pageCount: number) => [
+        {
+          text: `Desenvolvido por Cammove - Página ${currentPage} de ${pageCount}`,
+          alignment: 'center',
+          fontSize: 10,
+          margin: [0, 10, 0, 0],
+        },
       ],
     };
 
