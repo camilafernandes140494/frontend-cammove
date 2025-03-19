2caef00696f0665e2e138156d51811c1b1161acc
Camila <camilaferna140494@gmail.com>
2025-03-18 23:25:30 -0300

    WIP: working on screen layout

--- app/(assessments)/assessments.tsx
+++ app/(assessments)/assessments.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { ActivityIndicator, FlatList, View } from 'react-native';
 import {
   Button, Appbar,
-  SegmentedButtons
+  SegmentedButtons,
+  Text,
+  Card,
+  IconButton,
+  Chip
 } from 'react-native-paper';
 import FilterInput from '@/components/FilterInput';
 import { useStudent } from '../context/StudentContext';
-import SelectStudent from '@/components/SelectStudent';
 import { useUser } from '../UserContext';
+import { useQuery } from '@tanstack/react-query';
+import { getWorkoutsSummary } from '@/api/workout/workout.api';
+import { formatDate, getNextMonth } from '@/common/common';
 
 const Assessments = ({ navigation }: any) => {
   const [params, setParams] = useState<{ name: string }>({ name: '' });
@@ -15,6 +21,12 @@ const Assessments = ({ navigation }: any) => {
   const { user } = useUser();
   const [value, setValue] = useState('workouts');
 
+
+  const { data: assessmentsSummary, isLoading } = useQuery({
+    queryKey: ['getRelationship', params],
+    queryFn: () => getWorkoutsSummary(user?.id!, params),
+    enabled: !!user?.id,
+  });
 
   return (
     <View style={{ flex: 1 }}>
@@ -26,8 +38,8 @@ const Assessments = ({ navigation }: any) => {
       </Appbar.Header>
 
       <FlatList
-        data={['student']}
-        keyboardShouldPersistTaps="handled"
+        data={value === 'students' ? [] : assessmentsSummary}
+        keyExtractor={(item) => `${item.studentName}-${item.id}`}
         ListHeaderComponent={
           <View style={{ padding: 16 }}>
             <SegmentedButtons
@@ -42,14 +54,37 @@ const Assessments = ({ navigation }: any) => {
 
           </View>
         }
-        renderItem={() => <>
-          <View style={{ paddingHorizontal: 16 }}>
-            <SelectStudent
-              teacherId={user?.id!}
-              onSelect={(student) => { refetchStudent(student.studentId) }}
-              filterName={params?.name}
-            />
-          </View>
+        renderItem={({ item }) => <>
+          {
+            isLoading && value === 'assessments' ? <ActivityIndicator animating={true} style={{ marginTop: 16 }} size="large" color="#6200ea" /> : value === 'assessments' && <Card style={{ marginHorizontal: 16, borderRadius: 12, elevation: 5, marginBottom: 16 }}>
+              <Card.Title
+                title={item.studentName}
+                subtitle={`Criado em: ${formatDate(item.createdAt)}`}
+                right={(props) => (
+                  <IconButton
+                    {...props}
+                    icon="chevron-right"
+                    size={24}
+                    onPress={() => {
+                      refetchStudent(item.studentId);
+                      navigation.navigate('CreateAssessments', { workoutId: item.workoutId });
+                    }}
+                  />
+                )}
+                titleStyle={{ fontSize: 18, fontWeight: 'bold' }}
+                subtitleStyle={{ fontSize: 12, color: 'gray' }}
+              />
+              <Card.Content style={{ paddingVertical: 16 }}>
+                <Text variant="bodyMedium" style={{ fontSize: 14, marginBottom: 8 }}>
+                  Próxima atualização
+                </Text>
+                <Text variant="bodySmall" style={{ fontSize: 16, color: 'blue', fontWeight: '500', marginBottom: 20 }}>
+                  {getNextMonth(item.createdAt)}
+                </Text>
+                <Chip>{item.workoutType}</Chip>
+              </Card.Content>
+            </Card>
+          }
         </>
         }
 
