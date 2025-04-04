3e7037f0cc6a3063fadd71b54b7862ae4123fc3c
Camila <camilaferna140494@gmail.com>
2025-04-04 17:08:52 -0300

    fix: change from formik to react-hook-form

--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -1,56 +1,50 @@
 import React, { useState } from 'react';
-import { Button, Card, Chip, HelperText, Snackbar, Text, TextInput } from 'react-native-paper';
-import { FormikProvider, useFormik } from 'formik';
-import * as Yup from "yup";
+import { Button, Card, Snackbar, Text, TextInput } from 'react-native-paper';
 import { View } from 'react-native';
-// import DateTimePicker from '@react-native-community/datetimepicker';
 import { useUser } from '@/app/UserContext';
-import { PERMISSION } from '@/api/users/users.types';
+import { GENDER, PostUser } from '@/api/users/users.types';
 import UserList from './UserList';
 import { useNavigation } from '@react-navigation/native';
+import * as z from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { FormField } from './FormField';
 
 interface UserFormProps {
     color?: string;
-    onSubmit: (values: { name: string; gender: null; birthDate: string; image: string, permission: PERMISSION, email: string }) => void;
+    onSubmit: (values: Partial<PostUser>) => void;
 
 }
 
 const UserForm = ({ onSubmit }: UserFormProps) => {
     const [visible, setVisible] = useState(false);
-    const [showDatePicker, setShowDatePicker] = useState(false);
     const [showListTeacher, setShowListTeacher] = useState(false);
     const navigation = useNavigation();
 
-
     const { user, setUser } = useUser();
 
-    const validationSchema = Yup.object().shape({
-        name: Yup.string().required("Obrigatório"),
-        birthDate: Yup.string().required("Obrigatório"),
-        gender: Yup.string().required("Obrigatório"),
+    const schema = z.object({
+        name: z.string().nonempty("Obrigatório"),
+        birthDate: z.string().nonempty("Obrigatório"),
+        gender: z.string().nonempty("Obrigatório"),
     });
 
-    const formik = useFormik({
-        initialValues: {
-            name: "", birthDate: "", gender: null, permission: user?.permission || 'STUDENT', image: "", email: ''
+    const { control, handleSubmit, } = useForm<z.infer<typeof schema>>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            name: '',
+            birthDate: '',
+            gender: 'PREFER_NOT_TO_SAY'
         },
-        validationSchema: validationSchema,
-        enableReinitialize: true,
-        onSubmit: (value) => {
-            onSubmit(value), setUser(value), setShowListTeacher(true)
-        },
-        validateOnChange: false,
     });
 
-    const { handleSubmit, setFieldValue, touched, values, errors, handleChange, handleBlur } = formik;
-
-
-    const handleDateChange = (event: any, selectedDate: Date | undefined) => {
-        setShowDatePicker(false);
-        setFieldValue('birthDate', selectedDate)
+    const handleFormSubmit = (values: z.infer<typeof schema>) => {
+        onSubmit({ ...values, gender: values.gender as GENDER });
+        setUser(values);
+        setShowListTeacher(true);
     };
 
-    const [date, setDate] = useState(new Date())
+
     return (
         <View style={{ padding: 20 }}>
             <Snackbar
@@ -64,93 +58,47 @@ const UserForm = ({ onSubmit }: UserFormProps) => {
                 <Text>Não foi possível cadastrar</Text>
             </Snackbar>
 
-            {!showListTeacher && <FormikProvider value={formik}>
+            {!showListTeacher &&
                 <Card style={{ display: 'flex', flexDirection: 'column', gap: 20, padding: 20 }}>
-                    <TextInput
+
+                    <FormField
+                        control={control}
                         mode="flat"
+                        left={<TextInput.Icon icon="account-outline" />}
+                        name="name"
                         label="Qual é o seu nome?"
-                        value={values.name}
-                        onChangeText={handleChange("name")}
-                        onBlur={handleBlur("name")}
-                        error={touched.name && Boolean(errors.name)}
+                        type="text"
                     />
-                    {touched.name && errors.name && (
-                        <HelperText type="error">
-                            {errors.name}
-                        </HelperText>
-                    )}
+                    <FormField
+                        control={control}
+                        mode="flat"
+                        left={<TextInput.Icon icon="calendar" />}
+                        name="birthDate"
+                        type='birthDate'
+                        label="Data de nascimento"
+                        maxLength={10}
+                        keyboardType="numeric"
+                    />
 
-                    <Text variant='titleMedium' style={{ marginTop: 20 }}>Selecione sua data de nascimento</Text>
-
-                    {/* <DatePicker
-                        modal
-                        open={showDatePicker}
-                        date={date}
-                        onConfirm={(selectedDate) => {
-                            setDate(selectedDate)
-                            setShowDatePicker(false);
-                            setFieldValue('birthDate', selectedDate)
-                        }}
-                        onCancel={() => {
-                            setShowDatePicker(false)
-                        }}
-                    /> */}
-                    {/* {showDatePicker && (
-                        <DateTimePicker
-                            value={values.birthDate ? new Date(values.birthDate) : new Date()}
-                            mode="date"
-                            display="default"
-                            onChange={handleDateChange}
-                        />
-                    )} */}
-
-                    <Button mode='outlined' onPress={() => { setFieldValue('birthDate', '14/04/1994'), setShowDatePicker(true) }} style={{ marginTop: 20 }}
-                    >
-                        {values.birthDate ? new Date(values.birthDate).toLocaleDateString("pt-BR") : "Selecionar Data de Nascimento"}
-                    </Button>
-
-                    {touched.birthDate && errors.birthDate && (
-                        <HelperText type="error">
-                            {errors.birthDate}
-                        </HelperText>
-                    )}
-                    <Text variant='titleMedium' style={{ marginTop: 20 }}>Escolha o gênero com o qual se identifica</Text>
-                    <View style={{ flexDirection: 'row', flexWrap: 'wrap', gap: 10, marginTop: 20 }}>
-                        {
-                            [{ label: 'Masculino', value: 'MALE' },
+                    <Text variant='titleMedium' >Escolha o gênero com o qual se identifica</Text>
+                    <FormField
+                        control={control}
+                        name="gender"
+                        label="Gênero"
+                        type="chip"
+                        options={[
+                            { label: 'Masculino', value: 'MALE' },
                             { label: 'Feminino', value: 'FEMALE' },
                             { label: 'Outro', value: 'OTHER' },
-                            { label: 'Prefiro não me identificar', value: 'PREFER_NOT_TO_SAY' }].map((option, index) => (
-                                <Chip
-                                    key={index}
-                                    selected={values.gender === option.value}
-                                    onPress={() => setFieldValue('gender', option.value)}
-                                >
-                                    {option.label}
-                                </Chip>
-                            ))}
-                    </View>
-                    {touched.gender && errors.gender && (
-                        <HelperText type="error">
-                            {errors.gender}
-                        </HelperText>
-                    )}
-
-                    <Button
-                        mode="contained"
-                        onPress={handleSubmit as any}
-                        style={{
-                            borderRadius: 10,
-                            marginVertical: 20,
-                            marginTop: 30
-                        }}
-                        contentStyle={{ height: 50 }}
-                    >
+                            { label: 'Prefiro não me identificar', value: 'PREFER_NOT_TO_SAY' },
+                        ]}
+                    />
+                    <Button mode="contained" onPress={handleSubmit(handleFormSubmit)}>
                         Salvar
                     </Button>
 
                 </Card>
-            </FormikProvider>}
+            }
 
             {showListTeacher && user?.permission === 'STUDENT' &&
                 <UserList params={{ permission: 'TEACHER' }} navigation={navigation} />}
