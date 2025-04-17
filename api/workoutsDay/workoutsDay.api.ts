5df2ef81c0d373d9f93a55c76d049c16f990dfe4
Camila <camilaferna140494@gmail.com>
2025-04-16 22:23:21 -0300

    feat: add new endpoint

---
+++ api/workoutsDay/workoutsDay.api.ts
@@ -0,0 +1,24 @@
+// File: api.js
+import api from '../axios';
+
+export const logTrainingDay = async (studentId: string) => {
+  try {
+    const response = await api.post(`/workouts-day/students/${studentId}`);
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro ao criar treino:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
+
+export const getTrainingDays = async (studentId: string) => {
+  try {
+    const response = await api.get<string[]>(
+      `/workouts-day/students/${studentId}`,
+    );
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    console.error('Erro no login:', error);
+    throw error; // Propaga o erro para quem chamar a função
+  }
+};
