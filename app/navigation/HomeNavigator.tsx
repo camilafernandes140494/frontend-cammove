904fe624dd08fb05b596b0e476f39aa478a507f4
Camila <camilaferna140494@gmail.com>
2025-04-25 14:37:53 -0300

    feat: add new route

--- app/navigation/HomeNavigator.tsx
+++ app/navigation/HomeNavigator.tsx
@@ -5,6 +5,7 @@ import Home from '../(home)/home';
 import StudentProfile from '../(home)/studentProfile';
 import RegisterUserByTeacher from '../(home)/registerUserByTeacher';
 import UserList from '../(home)/UserList';
+import Reviews from '../(reviews)/review';
 
 
 const Stack = createStackNavigator();
@@ -19,6 +20,7 @@ function HomeNavigator() {
                 <Stack.Screen name="UserList" component={UserList} options={{ headerShown: false }} />
                 <Stack.Screen name="StudentProfile" component={StudentProfile} options={{ headerShown: false }} />
                 <Stack.Screen name="RegisterUserByTeacher" component={RegisterUserByTeacher} options={{ headerShown: false }} />
+                <Stack.Screen name="Reviews" component={Reviews} options={{ headerShown: false }} />
             </Stack.Navigator>
         </StudentProvider>
 
