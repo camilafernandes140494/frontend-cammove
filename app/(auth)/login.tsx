38a896f183526af316d3227b3a0454a4a7c63c94
Camila <camilaferna140494@gmail.com>
2025-04-29 17:25:09 -0300

    fix: remove formik

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,53 +1,66 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
 import {
     TextInput,
-    Button,
-    HelperText,
-    Text,
-    Snackbar,
+    Button, Text,
+    Snackbar
 } from 'react-native-paper';
 import { postLogin } from '@/api/auth/auth.api';
 import { useUser } from '../UserContext';
 import { useTheme } from '../ThemeContext';
 import { useNavigation } from '@react-navigation/native';
 import { getUserById } from '@/api/users/users.api';
+import { FormField } from '@/components/FormField';
+import * as z from "zod";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 
 const Login = () => {
     const navigation = useNavigation();
     const { setUser, login } = useUser();
     const { theme } = useTheme();
-    const [isLoadingButton, setIsLoadingButton] = useState(false);
 
     const [showPassword, setShowPassword] = useState(false);
     const [visible, setVisible] = useState(false);
 
-    const validationSchema = Yup.object().shape({
-        email: Yup.string()
-            .email('Por favor, insira um email válido')
-            .required('O email é obrigatório'),
-        password: Yup.string()
-            .min(6, 'A senha deve ter pelo menos 6 caracteres')
-            .required('A senha é obrigatória'),
+
+    const schema = z.object({
+        email: z.string().email('Por favor, insira um email válido').nonempty('O email é obrigatório'),
+        password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres').nonempty('A senha é obrigatória'),
+
     });
 
-    const handleLogin = async (values: { email: string; password: string }) => {
-        setIsLoadingButton(true)
-        try {
+
+    const { control, handleSubmit } = useForm<z.infer<typeof schema>>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            email: '',
+            password: ''
+        },
+    });
+
+    const mutation = useMutation({
+        mutationFn: async (values: { email: string; password: string }) => {
             const userCredential = await postLogin(values);
             const user = await getUserById(userCredential.user_id);
             console.log('...user', user)
             setUser({ id: userCredential.user_id, token: userCredential.uid, ...user, });
             login({ id: userCredential.user_id, token: userCredential.uid, ...user })
             navigation.navigate('Home' as never);
-        } catch (error) {
+        },
+        onSuccess: () => {
+            navigation.navigate('Exercises' as never)
+        },
+        onError: () => {
             setVisible(true);
-        } finally {
-            setIsLoadingButton(false)
         }
+    });
+    const onSubmit = async (data: any) => {
+        mutation.mutate(data);
     };
+
+
 
     return (
         <View style={{ flex: 1, backgroundColor: theme.colors.primary, }}>
@@ -89,98 +102,62 @@ const Login = () => {
                 >
                     <Text>Erro ao logar</Text>
                 </Snackbar>
-                <Formik
-                    initialValues={{ email: '', password: '' }}
-                    validationSchema={validationSchema}
-                    onSubmit={handleLogin}
+                <View
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 5,
+                    }}
                 >
-                    {({
-                        handleSubmit,
-                        handleChange,
-                        handleBlur,
-                        values,
-                        errors,
-                        touched,
-                    }) => (
-                        <View
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                gap: 5,
-                            }}
+                    <FormField control={control} mode="flat" name="email" label="E-mail" type="text" style={{
+                        backgroundColor: theme.background,
+                    }} />
+                    <FormField control={control} mode="flat" name="password" label="Senha" type="text"
+                        secureTextEntry={!showPassword}
+                        style={{
+                            backgroundColor: theme.background,
+                        }}
+                        right={
+                            <TextInput.Icon
+                                icon={showPassword ? 'eye-off' : 'eye'}
+                                onPress={() => setShowPassword(!showPassword)}
+                            />
+                        } />
+
+                    <Button
+                        style={{
+                            alignSelf: 'flex-end',
+                            marginBottom: 20,
+                        }}
+                        mode="text"
+                        onPress={() => console.log('Pressed')}
+                    >
+                        Esqueceu a senha?
+                    </Button>
+                    <Button
+                        mode="contained"
+                        loading={mutation.isPending}
+                        disabled={mutation.isPending}
+                        onPress={handleSubmit(onSubmit)}
+                        style={{
+                            borderRadius: 10,
+                            marginVertical: 20,
+                        }}
+                        contentStyle={{ height: 50 }}
+                    >
+                        ENTRAR
+                    </Button>
+
+                    <Text style={{ textAlign: 'center' }}>
+                        Não tem uma conta?{' '}
+                        <Text
+                            style={{ fontWeight: 'bold' }}
+                            onPress={() => navigation.navigate('CreateUser' as never)}
                         >
-                            <TextInput
-                                mode="flat"
-                                label="E-mail"
-                                value={values.email}
-                                onChangeText={handleChange('email')}
-                                onBlur={handleBlur('email')}
-                                style={{
-                                    backgroundColor: theme.background,
-                                }}
-                                error={touched.email && Boolean(errors.email)}
-                            />
-                            {touched.email && errors.email && (
-                                <HelperText type="error">{errors.email}</HelperText>
-                            )}
-                            <TextInput
-                                mode="flat"
-                                label="Senha"
-                                value={values.password}
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                secureTextEntry={!showPassword}
-                                right={
-                                    <TextInput.Icon
-                                        icon={showPassword ? 'eye-off' : 'eye'}
-                                        onPress={() => setShowPassword(!showPassword)}
-                                    />
-                                }
-                                style={{
-                                    backgroundColor: theme.background,
-                                }}
-                                error={touched.password && Boolean(errors.password)}
-                            />
-                            {touched.password && errors.password && (
-                                <HelperText type="error">{errors.password}</HelperText>
-                            )}
-
-                            <Button
-                                style={{
-                                    alignSelf: 'flex-end',
-                                    marginBottom: 20,
-                                }}
-                                mode="text"
-                                onPress={() => console.log('Pressed')}
-                            >
-                                Esqueceu a senha?
-                            </Button>
-                            <Button
-                                mode="contained"
-                                loading={isLoadingButton}
-                                disabled={isLoadingButton}
-                                onPress={handleSubmit as any}
-                                style={{
-                                    borderRadius: 10,
-                                    marginVertical: 20,
-                                }}
-                                contentStyle={{ height: 50 }}
-                            >
-                                ENTRAR
-                            </Button>
-
-                            <Text style={{ textAlign: 'center' }}>
-                                Não tem uma conta?{' '}
-                                <Text
-                                    style={{ fontWeight: 'bold' }}
-                                    onPress={() => navigation.navigate('CreateUser' as never)}
-                                >
-                                    Cadastre-se
-                                </Text>
-                            </Text>
-                        </View>
-                    )}
-                </Formik>
+                            Cadastre-se
+                        </Text>
+                    </Text>
+                </View>
             </View>
         </View>
     );
