4b50b72f902c83bd3958026f315686f36f346c0a
Camila <camilaferna140494@gmail.com>
2025-07-24 00:01:10 -0300

    feat: create context to register training

---
+++ context/WorkoutFormContext.tsx
@@ -0,0 +1,69 @@
+import type { WorkoutSuggestionResponse } from '@/api/openai/gemini.type';
+import type React from 'react';
+import { createContext, type ReactNode, useContext, useState } from 'react';
+
+type WorkoutFormontextType = {
+  step: number;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+  isGeneratedByIA: boolean | undefined;
+  setIsGeneratedByIA: React.Dispatch<React.SetStateAction<boolean | undefined>>;
+  workoutSuggestion: WorkoutSuggestionResponse | undefined;
+  setWorkoutSuggestion: React.Dispatch<
+    React.SetStateAction<WorkoutSuggestionResponse | undefined>
+  >;
+  nextStep: VoidFunction;
+  prevStep: VoidFunction;
+  goToStep: (step: number) => void;
+};
+
+const WorkoutFormContext = createContext<WorkoutFormontextType | undefined>(
+  undefined
+);
+
+export const WorkoutFormProvider = ({ children }: { children: ReactNode }) => {
+  const [step, setStep] = useState(1);
+  const [isGeneratedByIA, setIsGeneratedByIA] = useState<boolean>();
+
+  const [workoutSuggestion, setWorkoutSuggestion] =
+    useState<WorkoutSuggestionResponse>();
+
+  function nextStep() {
+    setStep((prev) => Math.min(prev + 1, 5));
+  }
+
+  function prevStep() {
+    setStep((prev) => Math.max(prev - 1, 1));
+  }
+
+  function goToStep(stepNumber: number) {
+    if (stepNumber >= 1 && stepNumber <= 5) {
+      setStep(stepNumber);
+    }
+  }
+
+  return (
+    <WorkoutFormContext.Provider
+      value={{
+        step,
+        setStep,
+        nextStep,
+        prevStep,
+        goToStep,
+        isGeneratedByIA,
+        setIsGeneratedByIA,
+        workoutSuggestion,
+        setWorkoutSuggestion,
+      }}
+    >
+      {children}
+    </WorkoutFormContext.Provider>
+  );
+};
+
+export const useWorkoutForm = () => {
+  const context = useContext(WorkoutFormContext);
+  if (!context) {
+    throw new Error('useWorkoutForm must be used within a WorkoutFormProvider');
+  }
+  return context;
+};
