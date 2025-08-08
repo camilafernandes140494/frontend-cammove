bb7443b726c223621ce8d4f0653d7c6d48466c93
Camila <camilaferna140494@gmail.com>
2025-08-07 22:34:10 -0300

    feat: add component to wait for loading

--- components/StudentCard.tsx
+++ components/StudentCard.tsx
@@ -1,53 +1,76 @@
+import { calculateAge, getGender, getInitials } from "@/common/common";
 import { useStudent } from "@/context/StudentContext";
 import { useTheme } from "@/context/ThemeContext";
-import { calculateAge, getGender, getInitials } from "@/common/common";
-import React, { ReactNode } from "react";
+import React, { type ReactNode } from "react";
 import { View } from "react-native";
 import { Avatar, Text } from "react-native-paper";
+import Skeleton from "./Skeleton";
 
 interface StudentCardProps {
-  children?: ReactNode;
+	children?: ReactNode;
 }
 
 const StudentCard = ({ children }: StudentCardProps) => {
-  const { theme } = useTheme();
-  const { student } = useStudent();
+	const { theme } = useTheme();
+	const { student, isLoading } = useStudent();
 
-  const LeftContent = (props: any) =>
-    student?.image ? (
-      <Avatar.Image
-        {...props}
-        size={60}
-        source={{ uri: student.image }}
-        style={{ backgroundColor: 'transparent', marginRight: 30 }}
-      />
-    ) : (
-      <Avatar.Text
-        {...props}
-        size={60}
-        label={getInitials(student?.name || '')}
-        style={{ marginRight: 16 }}
-      />
-    );
-  return <View style={{ backgroundColor: theme.colors.secondaryContainer, paddingVertical: 16 }}>
-    <View style={{ display: "flex", flexDirection: 'row', gap: 16, alignItems: 'center', paddingHorizontal: 16, marginBottom: 10 }}>
-      {student?.image
-        ? <Avatar.Image size={60} source={{ uri: student.image }} />
-        : <Avatar.Text label={getInitials(student?.name || '')} />}
+	return (
+		<View
+			style={{
+				backgroundColor: theme.colors.secondaryContainer,
+				paddingVertical: 16,
+			}}
+		>
+			{isLoading ? (
+				<SkeletonStudentCard />
+			) : (
+				<>
+					<View
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							gap: 16,
+							alignItems: "center",
+							paddingHorizontal: 16,
+							marginBottom: 10,
+						}}
+					>
+						{student?.image ? (
+							<Avatar.Image size={60} source={{ uri: student.image }} />
+						) : (
+							<Avatar.Text label={getInitials(student?.name || "")} />
+						)}
 
-      <View style={{ gap: 10 }} >
-        <Text style={{ fontSize: 16, }}>
-          {`${student?.name}`}
-        </Text>
-        <Text style={{ fontSize: 14, }}>
-          {`Gênero: ${getGender(student?.gender || '')} | ${calculateAge(student?.birthDate || '')} anos`}
-        </Text>
-      </View>
-    </View>
-    {children}
-  </View>;
+						<View style={{ gap: 10 }}>
+							<Text style={{ fontSize: 16 }}>{`${student?.name}`}</Text>
+							<Text style={{ fontSize: 14 }}>
+								{`Gênero: ${getGender(student?.gender || "")} | ${calculateAge(student?.birthDate || "")} anos`}
+							</Text>
+						</View>
+					</View>
+					{children}
+				</>
+			)}
+		</View>
+	);
 };
 
-
+const SkeletonStudentCard = () => (
+	<View
+		style={{
+			flexDirection: "row",
+			alignItems: "center",
+			paddingHorizontal: 16,
+			marginBottom: 10,
+			gap: 24,
+		}}
+	>
+		<Skeleton style={{ borderRadius: "50%", height: 48, width: 48 }} />
+		<View style={{ gap: 10 }}>
+			<Skeleton style={{ marginTop: 12, height: 24, width: 250 }} />
+			<Skeleton style={{ marginTop: 1, height: 24, width: 200 }} />
+		</View>
+	</View>
+);
 
 export default StudentCard;
