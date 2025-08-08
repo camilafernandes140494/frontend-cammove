8cbf588cf09bb73cd18bd6313136d8bccd45a055
Camila <camilaferna140494@gmail.com>
2025-08-07 22:53:18 -0300

    fix: remove FlatList

--- app/(assessments)/createAssessments.tsx
+++ app/(assessments)/createAssessments.tsx
@@ -1,102 +1,86 @@
-import { formatDate } from '@/common/common';
-import FilterInput from '@/components/FilterInput';
-import FormAssessments from '@/components/FormAssessments';
-import SelectStudent from '@/components/SelectStudent';
-import StudentCard from '@/components/StudentCard';
-import { useTheme } from '@/context/ThemeContext';
-import { useUser } from '@/context/UserContext';
-import { useNavigation } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import { FlatList, View } from 'react-native';
-import { Appbar, Button, Text } from 'react-native-paper';
-import { useStudent } from '../../context/StudentContext';
+import FilterInput from "@/components/FilterInput";
+import FormAssessments from "@/components/FormAssessments";
+import SelectStudent from "@/components/SelectStudent";
+import StudentCard from "@/components/StudentCard";
+import { useTheme } from "@/context/ThemeContext";
+import { useUser } from "@/context/UserContext";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { Appbar, Button, Text } from "react-native-paper";
+import { useStudent } from "../../context/StudentContext";
 export type RootStackParamList = {
-  Workouts: undefined;
-  CreateWorkout: { workoutId?: string; studentId?: string };
+	Workouts: undefined;
+	CreateWorkout: { workoutId?: string; studentId?: string };
 };
 
 type CreateAssessmentsProps = {
-  route: {
-    params?: {
-      assessmentsId?: string;
-      studentId?: string;
-    };
-  };
+	route: {
+		params?: {
+			assessmentsId?: string;
+			studentId?: string;
+		};
+	};
 };
 
 const CreateAssessments = ({ route }: CreateAssessmentsProps) => {
-  const navigation = useNavigation();
-  const { user } = useUser();
-  const { refetchStudent } = useStudent();
-  const [params, setParams] = useState('');
-  const { assessmentsId, studentId } = route.params || {};
-  const [newStudent, setNewStudent] = useState(!studentId);
-  const { theme } = useTheme();
-  const today = new Date();
-  const formattedDate = formatDate(today);
+	const navigation = useNavigation();
+	const { user } = useUser();
+	const { refetchStudent, student, resetStudent } = useStudent();
+	const [params, setParams] = useState("");
+	const { assessmentsId, studentId } = route.params || {};
+	const [newStudent, setNewStudent] = useState(!studentId);
+	const { theme } = useTheme();
 
-  useEffect(() => {
-    studentId && refetchStudent(studentId);
-  }, [studentId]);
+	useEffect(() => {
+		studentId && refetchStudent(studentId);
+	}, [studentId]);
 
-  return (
-    <>
-      <Appbar.Header>
-        <Appbar.BackAction
-          onPress={() => navigation.navigate('Assessments' as never)}
-        />
-        <Appbar.Content title="Cadastrar avaliação" />
-      </Appbar.Header>
-      {!assessmentsId && (
-        <StudentCard>
-          {assessmentsId && (
-            <Text
-              style={{ marginLeft: 16, color: theme.colors.outline }}
-              variant="bodySmall"
-            >
-              ID: {assessmentsId}
-            </Text>
-          )}
-          <Text
-            style={{ marginLeft: 16, color: theme.colors.outline }}
-            variant="bodySmall"
-          >{`Criado em: ${formattedDate}`}</Text>
-        </StudentCard>
-      )}
+	return (
+		<>
+			<Appbar.Header>
+				<Appbar.BackAction
+					onPress={() => {
+						navigation.navigate("Assessments" as never), resetStudent();
+					}}
+				/>
+				<Appbar.Content title="Cadastrar avaliação" />
+			</Appbar.Header>
 
-      <FlatList
-        contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }}
-        data={[{}]}
-        keyExtractor={() => 'header'}
-        renderItem={() => (
-          <>
-            {newStudent ? (
-              <View style={{ margin: 20 }}>
-                <Text variant="titleMedium">Escolha um aluno(a)</Text>
-                <FilterInput
-                  onChange={setParams}
-                  placeholder="Pesquisar aluno(a)"
-                />
-                <SelectStudent
-                  filterName={params}
-                  onSelect={(student) => refetchStudent(student.studentId)}
-                  teacherId={user?.id || ''}
-                />
-                <Button mode="contained" onPress={() => setNewStudent(false)}>
-                  Continuar
-                </Button>
-              </View>
-            ) : (
-              <>
-                <FormAssessments assessmentsId={assessmentsId} />
-              </>
-            )}
-          </>
-        )}
-        style={{ flex: 1, backgroundColor: theme.colors.background }}
-      />
-    </>
-  );
+			{student?.id !== user?.id && <StudentCard />}
+
+			<View
+				style={{
+					flex: 1,
+					backgroundColor: theme.colors.background,
+				}}
+			>
+				{newStudent ? (
+					<View style={{ marginHorizontal: 12, marginTop: 24 }}>
+						<Text variant="titleMedium">Escolha um aluno(a)</Text>
+						<FilterInput
+							onChange={setParams}
+							placeholder="Pesquisar aluno(a)"
+						/>
+						<SelectStudent
+							filterName={params}
+							onSelect={(student) => refetchStudent(student.studentId)}
+							teacherId={user?.id || ""}
+						/>
+						<Button
+							mode="contained"
+							onPress={() => setNewStudent(false)}
+							disabled={user?.id === student?.id}
+						>
+							Continuar
+						</Button>
+					</View>
+				) : (
+					<FormAssessments assessmentsId={assessmentsId} />
+				)}
+			</View>
+		</>
+	);
 };
 
 export default CreateAssessments;
