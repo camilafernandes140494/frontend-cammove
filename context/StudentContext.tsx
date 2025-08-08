f4acce2e23fc27765b86c445ea271a55e268c695
Camila <camilaferna140494@gmail.com>
2025-08-07 22:51:15 -0300

    feat: add resetStudent

--- context/StudentContext.tsx
+++ context/StudentContext.tsx
@@ -1,42 +1,64 @@
-import React, { createContext, useContext, ReactNode, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { getUserById } from '@/api/users/users.api';
-import { Users } from '@/api/users/users.types';
+import { getUserById } from "@/api/users/users.api";
+import type { Users } from "@/api/users/users.types";
+import { useQuery } from "@tanstack/react-query";
+import React, {
+	createContext,
+	type ReactNode,
+	useContext,
+	useState,
+} from "react";
 
 type StudentContextType = {
-  student: Users | undefined;
-  isLoading: boolean;
-  refetchStudent: (id: string) => void;
+	student: Users | undefined;
+	isLoading: boolean;
+	refetchStudent: (id: string) => void;
+	resetStudent: () => void;
 };
 
 const StudentContext = createContext<StudentContextType | undefined>(undefined);
 
-export const StudentProvider = ({ children, studentCode: initialStudentCode }: { children: ReactNode; studentCode: string, }) => {
-  const [studentCode, setStudentCode] = useState(initialStudentCode);
+export const StudentProvider = ({
+	children,
+	studentCode: initialStudentCode,
+}: {
+	children: ReactNode;
+	studentCode: string;
+}) => {
+	const [studentCode, setStudentCode] = useState(initialStudentCode);
 
-  const { data: student, isLoading: isLoadingStudent, isFetching: isFetchingStudent } = useQuery({
-    queryKey: ['student', studentCode],
-    queryFn: () => getUserById(studentCode),
-    enabled: !!studentCode,
-  });
+	const {
+		data: student,
+		isLoading: isLoadingStudent,
+		isFetching: isFetchingStudent,
+	} = useQuery({
+		queryKey: ["student", studentCode],
+		queryFn: () => getUserById(studentCode),
+		enabled: !!studentCode,
+	});
 
-  const refetchStudent = (newStudentCode: string) => {
-    setStudentCode(newStudentCode);
-  };
+	const refetchStudent = (newStudentCode: string) => {
+		setStudentCode(newStudentCode);
+	};
 
-  let isLoading = isLoadingStudent || isFetchingStudent
+	const resetStudent = () => {
+		setStudentCode(initialStudentCode);
+	};
 
-  return (
-    <StudentContext.Provider value={{ student, refetchStudent, isLoading }}>
-      {children}
-    </StudentContext.Provider>
-  );
+	const isLoading = isLoadingStudent || isFetchingStudent;
+
+	return (
+		<StudentContext.Provider
+			value={{ student, refetchStudent, isLoading, resetStudent }}
+		>
+			{children}
+		</StudentContext.Provider>
+	);
 };
 
 export const useStudent = () => {
-  const context = useContext(StudentContext);
-  if (!context) {
-    throw new Error('useStudent must be used within a StudentProvider');
-  }
-  return context;
+	const context = useContext(StudentContext);
+	if (!context) {
+		throw new Error("useStudent must be used within a StudentProvider");
+	}
+	return context;
 };
