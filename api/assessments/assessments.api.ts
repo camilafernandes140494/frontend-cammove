6d4d905b1841c7f0a97a6f5b463f011d5c63bee8
Camila <camilaferna140494@gmail.com>
2025-08-13 22:44:20 -0300

    chore: debug with console.log

--- api/assessments/assessments.api.ts
+++ api/assessments/assessments.api.ts
@@ -1,98 +1,101 @@
-import api from '../axios';
-import { AssessmentData, BasicAssessmentItem } from './assessments.types';
+import api from "../axios";
+import type { AssessmentData, BasicAssessmentItem } from "./assessments.types";
 
 export const postAssessments = async (
-  teacherId: string,
-  studentId: string,
-  params: Partial<AssessmentData>,
+	teacherId: string,
+	studentId: string,
+	params: Partial<AssessmentData>,
 ) => {
-  try {
-    const response = await api.post(
-      `/physical-assessment/teachers/${teacherId}/students/${studentId}`,
-      params,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro ao criar treino:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.post(
+			`/physical-assessment/teachers/${teacherId}/students/${studentId}`,
+			params,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro ao criar treino:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const patchAssessments = async (
-  assessmentsId: string,
-  teacherId: string,
-  studentId: string,
-  params: Partial<AssessmentData>,
+	assessmentsId: string,
+	teacherId: string,
+	studentId: string,
+	params: Partial<AssessmentData>,
 ) => {
-  try {
-    const response = await api.patch(
-      `/physical-assessment/${assessmentsId}/students/${studentId}/teacher/${teacherId}`,
-      params,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro ao criar treino:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.patch(
+			`/physical-assessment/${assessmentsId}/students/${studentId}/teacher/${teacherId}`,
+			params,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro ao criar treino:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const deleteAssessmentsByStudentId = async (
-  assessmentsId: string,
-  studentId: string,
-  teacherId: string,
+	assessmentsId: string,
+	studentId: string,
+	teacherId: string,
 ) => {
-  try {
-    const response = await api.delete(
-      `/physical-assessment/${assessmentsId}/students/${studentId}/teacher/${teacherId}`,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.delete(
+			`/physical-assessment/${assessmentsId}/students/${studentId}/teacher/${teacherId}`,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro pra deletar avaliação:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const getAssessmentsByStudentIdAndAssessmentsId = async (
-  assessmentsId: string,
-  studentId: string,
+	assessmentsId: string,
+	studentId: string,
 ) => {
-  try {
-    const response = await api.get<AssessmentData>(
-      `/physical-assessment/${assessmentsId}/students/${studentId}`,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.get<AssessmentData>(
+			`/physical-assessment/${assessmentsId}/students/${studentId}`,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error(
+			"Erro no retorno do getAssessmentsByStudentIdAndAssessmentsId:",
+			error,
+		);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const getAssessmentsByStudentId = async (studentId: string) => {
-  try {
-    const response = await api.get<BasicAssessmentItem[]>(
-      `/physical-assessment/students/${studentId}`,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.get<BasicAssessmentItem[]>(
+			`/physical-assessment/students/${studentId}`,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no getAssessmentsByStudentId:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const getAssessmentsSummary = async (
-  teacherId: string,
-  params?: Record<string, string>,
+	teacherId: string,
+	params?: Record<string, string>,
 ) => {
-  try {
-    const response = await api.get<any[]>(
-      `/physical-assessment/teachers/${teacherId}/summary`,
-      {
-        params,
-      },
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.get<any[]>(
+			`/physical-assessment/teachers/${teacherId}/summary`,
+			{
+				params,
+			},
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no getAssessmentsSummary:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
--- api/workout/workout.api.ts
+++ api/workout/workout.api.ts
@@ -1,119 +1,119 @@
 // File: api.js
-import api from '../axios';
-import {
-  getWorkoutsSummaryResponse,
-  WorkoutData,
-  WorkoutDataByIdStudent,
-} from './workout.types';
+import api from "../axios";
+import type {
+	getWorkoutsSummaryResponse,
+	WorkoutData,
+	WorkoutDataByIdStudent,
+} from "./workout.types";
 
 export const postWorkout = async (
-  teacherId: string,
-  studentId: string,
-  params: WorkoutData,
+	teacherId: string,
+	studentId: string,
+	params: WorkoutData,
 ) => {
-  try {
-    const response = await api.post(
-      `/workouts/teachers/${teacherId}/students/${studentId}`,
-      params,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro ao criar treino:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.post(
+			`/workouts/teachers/${teacherId}/students/${studentId}`,
+			params,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro ao criar treino:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const patchWorkout = async (
-  workoutId: string,
-  teacherId: string,
-  studentId: string,
-  params: Partial<WorkoutData>,
+	workoutId: string,
+	teacherId: string,
+	studentId: string,
+	params: Partial<WorkoutData>,
 ) => {
-  try {
-    const response = await api.patch(
-      `/workouts/${workoutId}/students/${studentId}/teacher/${teacherId}`,
-      params,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro ao criar treino:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.patch(
+			`/workouts/${workoutId}/students/${studentId}/teacher/${teacherId}`,
+			params,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro ao criar treino:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const getWorkoutsSummary = async (
-  teacherId: string,
-  params?: Record<string, string>,
+	teacherId: string,
+	params?: Record<string, string>,
 ) => {
-  try {
-    const response = await api.get<getWorkoutsSummaryResponse[]>(
-      `/workouts/teachers/${teacherId}/summary`,
-      {
-        params,
-      },
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.get<getWorkoutsSummaryResponse[]>(
+			`/workouts/teachers/${teacherId}/summary`,
+			{
+				params,
+			},
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no getWorkoutsSummary:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const getWorkoutsByStudentId = async (studentId: string) => {
-  try {
-    const response = await api.get<WorkoutDataByIdStudent[]>(
-      `/workouts/students/${studentId}`,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.get<WorkoutDataByIdStudent[]>(
+			`/workouts/students/${studentId}`,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no getWorkoutsByStudentId:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const deleteWorkoutsByStudentId = async (
-  workoutId: string,
-  studentId: string,
-  teacherId: string,
+	workoutId: string,
+	studentId: string,
+	teacherId: string,
 ) => {
-  try {
-    const response = await api.delete(
-      `/workouts/${workoutId}/students/${studentId}/teacher/${teacherId}`,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.delete(
+			`/workouts/${workoutId}/students/${studentId}/teacher/${teacherId}`,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no deleteWorkoutsByStudentId:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const duplicateWorkout = async (
-  workoutId: string,
-  studentId: string,
-  teacherId: string,
+	workoutId: string,
+	studentId: string,
+	teacherId: string,
 ) => {
-  try {
-    const response = await api.post(
-      `/workouts/${workoutId}/students/${studentId}/teachers/${teacherId}/duplicate`,
-    );
-    return response.data;
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error;
-  }
+	try {
+		const response = await api.post(
+			`/workouts/${workoutId}/students/${studentId}/teachers/${teacherId}/duplicate`,
+		);
+		return response.data;
+	} catch (error) {
+		console.error("Erro no duplicateWorkout:", error);
+		throw error;
+	}
 };
 
 export const getWorkoutByStudentIdAndWorkoutId = async (
-  workoutsId: string,
-  studentId: string,
+	workoutsId: string,
+	studentId: string,
 ) => {
-  try {
-    const response = await api.get<WorkoutData>(
-      `/workouts/${workoutsId}/students/${studentId}`,
-    );
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.get<WorkoutData>(
+			`/workouts/${workoutsId}/students/${studentId}`,
+		);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no getWorkoutByStudentIdAndWorkoutId:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
