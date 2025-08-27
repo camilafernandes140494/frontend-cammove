cb7abaf22c8407d45399b1c54c67bbb84fe7dc10
Camila <camilaferna140494@gmail.com>
2025-08-26 22:05:05 -0300

    feat(types): add video field to Exercise type

--- api/exercise/exercise.types.ts
+++ api/exercise/exercise.types.ts
@@ -1,16 +1,17 @@
 export type Exercise = {
-  name: string;
-  description: string;
-  category?: string;
-  muscleGroup?: string[];
-  images?: string[];
-  createdAt?: string;
-  updatedAt?: string;
-  deletedAt?: string;
-  id?: string;
+	name: string;
+	description: string;
+	category?: string;
+	muscleGroup?: string[];
+	images?: string[];
+	video?: string;
+	createdAt?: string;
+	updatedAt?: string;
+	deletedAt?: string;
+	id?: string;
 };
 
 export type Upload = {
-  file: Blob;
-  folder: 'exercises' | 'fallow-up';
+	file: Blob;
+	folder: "exercises" | "fallow-up";
 };
