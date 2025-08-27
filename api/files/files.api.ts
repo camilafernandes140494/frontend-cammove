64d5aca21ecc84924103489b656f7d65b71d2a84
Camila <camilaferna140494@gmail.com>
2025-08-26 22:04:46 -0300

    feat(api): add postVideos function for video upload

--- api/files/files.api.ts
+++ api/files/files.api.ts
@@ -1,35 +1,49 @@
-import api from '../axios';
+import api from "../axios";
 
 export const postFiles = async (folder: string, formData: FormData) => {
-  try {
-    const response = await api.post(`/files/${folder}/upload`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Erro ao enviar arquivo:', error);
-    throw error;
-  }
+	try {
+		const response = await api.post(`/files/${folder}/upload`, formData, {
+			headers: {
+				"Content-Type": "multipart/form-data",
+			},
+		});
+		return response.data;
+	} catch (error) {
+		console.error("Erro ao enviar arquivo:", error);
+		throw error;
+	}
 };
 
 export const getFiles = async (folder: string, fileId: string) => {
-  try {
-    const response = await api.get(`/files/${folder}/${fileId}`);
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.get(`/files/${folder}/${fileId}`);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no login:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
 
 export const deleteFiles = async (folder: string, fileId: string) => {
-  try {
-    const response = await api.delete(`/files/${folder}/${fileId}`);
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.delete(`/files/${folder}/${fileId}`);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no login:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
+};
+
+export const postVideos = async (folder: string, formData: FormData) => {
+	try {
+		const response = await api.post(`/files/${folder}/upload/video`, formData, {
+			headers: {
+				"Content-Type": "multipart/form-data",
+			},
+		});
+		return response.data;
+	} catch (error) {
+		console.error("Erro ao enviar arquivo:", error);
+		throw error;
+	}
 };
