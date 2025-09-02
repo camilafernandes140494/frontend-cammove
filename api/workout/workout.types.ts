b2665c63236ee7eb29bbd3777e07eac383ea7312
Camila <camilaferna140494@gmail.com>
2025-09-01 22:09:29 -0300

    Renamed workoutType property to nameWorkout to better reflect workout name.

--- api/workout/workout.types.ts
+++ api/workout/workout.types.ts
@@ -1,39 +1,40 @@
-import type { Exercise } from '../exercise/exercise.types';
+import type { Exercise } from "../exercise/exercise.types";
 
 export type getWorkoutsSummaryResponse = {
-  id: string;
-  workoutId: string;
-  createdAt: string;
-  expireAt: string;
-  workoutType: string;
-  studentName: string;
-  studentId: string;
+	id: string;
+	workoutId: string;
+	createdAt: string;
+	expireAt: string;
+	workoutType: string;
+	nameWorkout: string;
+	studentName: string;
+	studentId: string;
 };
 
 export type ExerciseWorkout = {
-  exerciseId: Exercise;
-  repetitions: string;
-  sets: string;
-  restTime: string;
-  observations: string;
-  category?: string;
+	exerciseId: Exercise;
+	repetitions: string;
+	sets: string;
+	restTime: string;
+	observations: string;
+	category?: string;
 };
 export type WorkoutData = {
-  createdAt?: string;
-  updatedAt?: string;
-  deletedAt?: string;
-  exercises: ExerciseWorkout[];
-  type: string;
-  studentName: string;
-  studentId: string;
-  nameWorkout: string;
-  level?: string;
-  muscleGroup?: string[];
+	createdAt?: string;
+	updatedAt?: string;
+	deletedAt?: string;
+	exercises: ExerciseWorkout[];
+	type: string;
+	studentName: string;
+	studentId: string;
+	nameWorkout: string;
+	level?: string;
+	muscleGroup?: string[];
 };
 
 export type WorkoutDataByIdStudent = {
-  id: string;
-  createdAt: string;
-  type?: string;
-  nameWorkout: string;
+	id: string;
+	createdAt: string;
+	type?: string;
+	nameWorkout: string;
 };
