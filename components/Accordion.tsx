d80fcf9f50b31164b07cdaa816d117827628072e
Camila <camilaferna140494@gmail.com>
2025-09-01 22:07:16 -0300

    feat: enhance Accordion component with theming and expand/collapse state

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,4 +1,7 @@
+import { useTheme } from "@/context/ThemeContext";
 import type * as React from "react";
+import { useState } from "react";
+import { View } from "react-native";
 import { List } from "react-native-paper";
 import type { IconSource } from "react-native-paper/lib/typescript/components/Icon";
 
@@ -15,14 +18,37 @@ export const Accordion = <T,>({
 	description,
 	children,
 	icon = "folder",
+	defaultExpanded = false,
 }: AccordionProps<T>) => {
+	const [expanded, setExpanded] = useState(defaultExpanded);
+	const { theme } = useTheme();
+
 	return (
-		<List.Accordion
-			title={title}
-			description={description}
-			left={(props) => <List.Icon {...props} icon={icon} />}
+		<View
+			style={{
+				borderWidth: 1,
+				borderColor: expanded ? theme.colors.primary : "#ccc",
+				borderRadius: 8,
+				marginVertical: 12,
+				overflow: "hidden",
+			}}
 		>
-			<List.Item title={() => children} />
-		</List.Accordion>
+			<List.Accordion
+				expanded={expanded}
+				onPress={() => setExpanded(!expanded)}
+				title={title}
+				description={description}
+				left={(props) => <List.Icon {...props} icon={icon} />}
+			>
+				<View
+					style={{
+						paddingRight: 40,
+						paddingVertical: 24,
+					}}
+				>
+					{children}
+				</View>
+			</List.Accordion>
+		</View>
 	);
 };
