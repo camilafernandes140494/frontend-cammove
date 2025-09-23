47e6a49bbe420e988211c0a34c2902811049d9b4
Camila <camilaferna140494@gmail.com>
2025-09-23 20:57:07 -0300

    feat: add teacherData to MyTeacherContext

--- context/MyTeacherContext.tsx
+++ context/MyTeacherContext.tsx
@@ -1,35 +1,46 @@
-import React, { createContext, useContext, ReactNode } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { getMyTeacher } from '@/api/relationships/relationships.api';
-import { useUser } from './UserContext';
-import { Relationship } from '@/api/relationships/relationships.types';
+import { getMyTeacher } from "@/api/relationships/relationships.api";
+import type { Relationship } from "@/api/relationships/relationships.types";
+import { getUserById } from "@/api/users/users.api";
+import type { Users } from "@/api/users/users.types";
+import { useQuery } from "@tanstack/react-query";
+import React, { createContext, type ReactNode, useContext } from "react";
+import { useUser } from "./UserContext";
 
 type MyTeacherContextType = {
-  teacher: Relationship | undefined;
+	teacher: Relationship | undefined;
+	teacherData?: Users;
 };
 
-const MyTeacherContext = createContext<MyTeacherContextType | undefined>(undefined);
+const MyTeacherContext = createContext<MyTeacherContextType | undefined>(
+	undefined,
+);
 
-export const MyTeacherProvider = ({ children }: { children: ReactNode; }) => {
-  const { user } = useUser();
+export const MyTeacherProvider = ({ children }: { children: ReactNode }) => {
+	const { user } = useUser();
 
-  const { data: teacher } = useQuery({
-    queryKey: ['getMyTeacher', user?.id],
-    queryFn: () => getMyTeacher(user?.id || ''),
-    enabled: !!user?.id,
-  });
+	const { data: teacher } = useQuery({
+		queryKey: ["getMyTeacher", user?.id],
+		queryFn: () => getMyTeacher(user?.id || ""),
+		enabled: !!user?.id,
+	});
 
-  return (
-    <MyTeacherContext.Provider value={{ teacher }}>
-      {children}
-    </MyTeacherContext.Provider>
-  );
+	const { data: teacherData } = useQuery({
+		queryKey: ["getMyTeacherIdDevice", teacher?.teacherId],
+		queryFn: () => getUserById(teacher?.teacherId || ""),
+		enabled: !!teacher?.teacherId,
+	});
+
+	return (
+		<MyTeacherContext.Provider value={{ teacher, teacherData }}>
+			{children}
+		</MyTeacherContext.Provider>
+	);
 };
 
 export const useMyTeacher = () => {
-  const context = useContext(MyTeacherContext);
-  if (!context) {
-    throw new Error('useMyTeacher must be used within a MyTeacherProvider');
-  }
-  return context;
+	const context = useContext(MyTeacherContext);
+	if (!context) {
+		throw new Error("useMyTeacher must be used within a MyTeacherProvider");
+	}
+	return context;
 };
