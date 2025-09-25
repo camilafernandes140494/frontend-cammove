aad3e3c8cf2e3eb2594c83816e46bd03df891240
Camila <camilaferna140494@gmail.com>
2025-09-24 23:52:27 -0300

    chore: no changes in postEmail function

--- api/email/email.api.ts
+++ api/email/email.api.ts
@@ -1,12 +1,12 @@
-import api from '../axios';
-import { PostEmail } from './email.types';
+import api from "../axios";
+import type { PostEmail } from "./email.types";
 
 export const postEmail = async (params: PostEmail) => {
-  try {
-    const response = await api.post('/email/send', params);
-    return response.data; // Retorna os dados da resposta
-  } catch (error) {
-    console.error('Erro no login:', error);
-    throw error; // Propaga o erro para quem chamar a função
-  }
+	try {
+		const response = await api.post("/email/send", params);
+		return response.data; // Retorna os dados da resposta
+	} catch (error) {
+		console.error("Erro no login:", error);
+		throw error; // Propaga o erro para quem chamar a função
+	}
 };
